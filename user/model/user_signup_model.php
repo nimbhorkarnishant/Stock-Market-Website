8fa71fde57c0654f9b2c84d8bb1d199fa980c55d
Nishant Nimbhorkar <36923081+nimbhorkarnishant@users.noreply.github.com>
2019-12-17 12:31:10 +0530

    Update user_signup_model.php

--- user/model/user_signup_model.php
+++ user/model/user_signup_model.php
@@ -34,12 +34,11 @@ class user_signup{
     $mail->isSMTP();                                      // Set mailer to use SMTP
     $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
     $mail->SMTPAuth = true;                               // Enable SMTP authentication
-    $mail->Username = 'wtproject1122@gmail.com';                 // SMTP username
-    $mail->Password = 'wtproject@12345';                           // SMTP password
+    $mail->Username = 'abc@gmail.com';                 // SMTP username
+    $mail->Password = 'password';                           // SMTP password
     $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
     $mail->Port = 465;                                    // TCP port to connect to
 
-    $mail->setFrom('wtproject.org', 'WT Project');
     $mail->addAddress($toemail);     // Add a recipient
 
     $mail->isHTML(true);                                  // Set email format to HTML
